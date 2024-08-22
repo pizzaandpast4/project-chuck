08015cde874931b4464c70c678a758202e42fc06
pizzaandpast4 <dmitrij.fiodorov@gmail.com>
2024-08-22 11:46:10 +0300

    ok

---
+++ script.js
@@ -0,0 +1,11 @@
+const btnDOM = document.querySelector("button");
+const p = document.querySelector("p");
+const url = "https://api.chucknorris.io/jokes/random";
+
+btnDOM.addEventListener("click", getJoke);
+
+function getJoke() {
+    fetch(url)
+        .then(res => res.json())
+        .then(data => p.innerHTML = data.value)
+}
